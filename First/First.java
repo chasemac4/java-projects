c0e607d812b9df0a03c3b15eeae557ef4b220d26
Chase McIntosh <cmcintosh339@gmail.com>
2014-10-15 16:03:04 -0700

    This is my first time on Git

---
+++ First/First.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class First here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class First
+{
+    public static void main (String args [])
+    {
+        System.out.println ("Hello");
+        
+    }
+}
