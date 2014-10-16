6e89d5acb950a460ea1c2d232e0427e5f993b88f
Ethan Lee <ethanlee@ethansites.com>
2014-10-15 17:18:34 -0700

    Added Triangle inequality law exception

--- First/First.java
+++ First/First.java
@@ -1,18 +1,21 @@
 
 /**
- * Write a description of class First here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * First.java - just does a triangle thing.
+ * Find me on GitHub! https://github.com/chasemac4/java-projects
+ *
+ * @author Chase McIntosh, Ethan Lee
+ * @version 1.0.0
  */
+
 import java.util.Scanner;
 import java.text.DecimalFormat;
+
 public class First
 {
     public static void main (String args [])
     {
-        Scanner scan = new Scanner (System.in);
-        DecimalFormat fmt = new DecimalFormat ("###");
+        Scanner scan = new Scanner(System.in);
+        DecimalFormat fmt = new DecimalFormat("#.###");
         
         double a;
         double b;
@@ -29,9 +32,14 @@ public class First
         c = scan.nextDouble();
         
         s = (a + b + c) / 2;
-        System.out.println("Here is the area of the triangle: "
-            + Math.sqrt(s * (s - a) * (s - b) * (s - c)));  
         
+        /* Triangle Inequality Law */
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            System.out.println("Aw shucks, this doesn't make a triangle.");
+        } else {
+            System.out.println("Here is the area of the triangle: "
+                               + fmt.format(Math.sqrt(s * (s - a) * (s - b) * (s - c))));
+        }
         
         
         
