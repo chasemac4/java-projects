8692b116fe848630a3465911c63ee96b80a2957a
Chase McIntosh <cmcintosh339@gmail.com>
2014-10-15 17:05:00 -0700

    Edits

--- First/First.java
+++ First/First.java
@@ -6,11 +6,34 @@
  * @version (a version number or a date)
  */
 import java.util.Scanner;
+import java.text.DecimalFormat;
 public class First
 {
     public static void main (String args [])
     {
-        System.out.println ("Hello");
+        Scanner scan = new Scanner (System.in);
+        DecimalFormat fmt = new DecimalFormat ("###");
+        
+        double a;
+        double b;
+        double c;
+        double s;
+        
+        System.out.println("Please enter in the first triangle length: ");
+        a = scan.nextDouble();
+        
+        System.out.println("Please enter in the second triangle length: ");
+        b = scan.nextDouble();
+        
+        System.out.println("Please enter in the third triangle length: ");
+        c = scan.nextDouble();
+        
+        s = (a + b + c) / 2;
+        System.out.println("Here is the area of the triangle: "
+            + Math.sqrt(s * (s - a) * (s - b) * (s - c)));  
+        
+        
+        
         
     }
 }
